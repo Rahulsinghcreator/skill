06be9ac2018e1bec6971822fd15ce592f2838f7e
Hitmanjod <142624499+Hitmanjod@users.noreply.github.com>
2024-01-04 20:57:07 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = 
-api_hash = ""
-bot_token = "6930088546:AAFOkh1wlahXpi-gPaN2GozXjSLo_nSHSYg"
+api_id = 9344337
+api_hash = "7e55bf98380e416d5de1c4c567395a32"
+bot_token = "6348631191:AAHAOHOL5i46ibm0AsNuqiVlT7xhKrbsov0"
