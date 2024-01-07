703f31531c1a4776e77859ea957a06fb053d59f0
Hitmanjod <142624499+Hitmanjod@users.noreply.github.com>
2024-01-07 18:00:51 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
-api_id = 9344337
-api_hash = "7e55bf98380e416d5de1c4c567395a32"
-bot_token = "6348631191:AAHAOHOL5i46ibm0AsNuqiVlT7xhKrbsov0"
+api_id = 
+api_hash = 
+bot_token = 
 AUTH_USERS = 6040965491
